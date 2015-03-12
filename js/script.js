461965838ccacebc65d9be149072378d6dd6d9f8
Mohd Zaki <mohdzaki04@gmail.com>
2015-03-12 09:23:36 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $("[data-toggle=popover]").popover();
 //            $("[name='my-checkbox']").bootstrapSwitch();
 $(document).ready(function () {
+
+    /*navbar*/
     $('ul.nav li.dropdown').hover(function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(100);
     }, function () {
         $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(50);
+    });
+
+    /*Carousel*/
+    $('#myCarousel').carousel({
+        interval: 10000
+    })
+
+    $('#myCarousel').on('slid.bs.carousel', function () {
+        //alert("slid");
     });
 });
